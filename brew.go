f2b6e649b61a114668565313f8be455f04d41788
Charlie Chiang <charlie_c_0129@outlook.com>
2025-02-20 21:56:06 +0800

    feat: add error when users attempt to call install command on homebrew-installed batt
    #25
    
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- brew.go
+++ brew.go
@@ -2,4 +2,34 @@
 
 package main
 
-var hideInstallCommands = true
+import (
+	"errors"
+
+	"github.com/spf13/cobra"
+)
+
+// NewInstallCommand .
+func NewInstallCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:    "install",
+		Hidden: true,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return errors.New("install command is not available on Homebrew-installed batt. Use `sudo brew services start batt` instead.")
+		},
+	}
+
+	cmd.Flags().Bool("allow-non-root-access", false, "Allow non-root users to access batt daemon.")
+
+	return cmd
+}
+
+// NewUninstallCommand .
+func NewUninstallCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:    "uninstall",
+		Hidden: true,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return errors.New("uninstall command is not available on Homebrew-installed batt. Use `sudo brew services stop batt` instead.")
+		},
+	}
+}
