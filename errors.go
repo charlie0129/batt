7d10e428f546da805953ae755993fe72da703ee0
pichxyaponn <pichxyaponn@gmail.com>
2025-06-22 00:24:44 +0700

    fix: linter

--- errors.go
+++ errors.go
@@ -3,7 +3,9 @@ package main
 import "errors"
 
 var (
+	// ErrDaemonNotRunning is returned when the daemon is not running
 	ErrDaemonNotRunning = errors.New("daemon not running")
 
+	// ErrPermissionDenied is returned when the user does not have permission to perform the requested action
 	ErrPermissionDenied = errors.New("permission denied")
 )
