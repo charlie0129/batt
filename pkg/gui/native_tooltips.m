c799aeef15d0eff0b21cc9f03e86b8f27be3b90b
Charlie Chiang <charlie_c_0129@outlook.com>
2026-07-15 11:48:53 +0800

    chore: update tooltip
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- pkg/gui/native_tooltips.m
+++ pkg/gui/native_tooltips.m
@@ -56,5 +56,5 @@ void BattApplyTooltips(BattMenuController *controller) {
     SetTooltip(controller, BattItemQuit,
         @"Quit the batt menubar app, but keep the batt daemon running.\n\n"
          "Since the batt daemon is still running, batt can continue to control charging. This is useful if you don't want the menubar icon to show up, but still want to use batt. When the client is not running, you can change batt settings using the command line interface (batt). To prevent the menubar app from starting at login, you can remove it in System Settings -> General -> Login Items & Extensions -> remove batt.app from the list (do NOT remove the batt daemon).\n\n"
-         "If you want to stop batt completely (menubar app and the daemon), you can use the \"Disable Charge Limit\" command. To uninstall, you can use the \"Uninstall Daemon\" command in the Advanced menu.");
+         "If you want batt to stop controlling charging, you can use the \"Disable Charge Limit\" command. To uninstall, you can use the \"Uninstall Daemon\" command in the Advanced menu.");
 }
--- pkg/gui/strings.go
+++ pkg/gui/strings.go
@@ -7,6 +7,6 @@ const (
 
 Since the batt daemon is still running, batt can continue to control charging. This is useful if you don't want the menubar icon to show up, but still want to use batt. When the client is not running, you can change batt settings using the command line interface (batt). To prevent the menubar app from starting at login, you can remove it in System Settings -> General -> Login Items & Extensions -> remove batt.app from the list (do NOT remove the batt daemon).
 
-If you want to stop the batt daemon, you can use the "Disable Charge Limit" command. To uninstall, you can use the "Uninstall Daemon" command in the Advanced menu.`
+If you want batt to stop controlling charging, you can use the "Disable Charge Limit" command. To uninstall, you can use the "Uninstall Daemon" command in the Advanced menu.`
 	quitTooltipNotInstalled = `Quit the batt menubar app.`
 )
